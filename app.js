70ff35d1068220ff455b70f51c7b6a2843ab2531
Arpan <rockarpan31@gmail.com>
2024-01-18 18:31:50 +0545

    JASS

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const URL ="https://cat-fact.herokuapp.com/facts";
-const factPara = document.querySelector("#fact");
-const btn = document.querySelector("#btn");
+const BASE_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd/jpy.json"
 
 
-const getFacts = async() =>{
-    console.log("getting data......");
-     let response = await fetch(URL);
-     console.log(response);
-      let data = await response.json();
-      factPara.innerText = data[2].text;
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = documnt.querySelector("form button");
 
-};
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+         if(select.name ==="from" && currCode === "USD"){
+            newOption.selected="selected";
+         }else if (select.name === "to" && currCode === "NPR"){
+            newOption.selected= "selected";
+         }
+        select.append(newOption);
+    }
+     select.addEventListener("change",(evt)  => {
+          updateFlag(evt.target);
+     });
+}
 
 
+    const updateFlag = (element) =>{
+       let currCode = element.value;
+       let countryCode = countryList[currCode];
+       let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
+       let img = element.parentElement.querySelector("img");
+       img.src = newSrc;
 
-btn.addEventListener("click", getFacts);
+    }
